02967c531af3df70c4bde1b7c0e43034da95c9eb
myjohnson23 <myjohnson@gmail.com>
2015-07-23 14:33:03 -0500

    adding views

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@ Bundler.require()
 
 # http://localhost/
 get '/' do
-  {:name => 'Darth Vader', :message => 'The force is string with this one'}.to_json
+  erb :index # erb says! go fin
 end
 
-# http://localhost/api/awesome/
-get '/api/awesome' do
-  {:name => 'Myron Johnson',
-    :age => '41',
-    :favorite_color => 'gray and blue',
-    :favorite_sport => 'Real Football',
-    :favorite_jam => 'Computer Blue',
-    :what_am_i_gonna_kick => 'Web Developments Ass'
-  }.to_json
-end
+# #http://localhost/api/awesome/
+# get '/api/awesome' do
+#   {:name => 'Myron Johnson',
+#     :age => '41',
+#     :favorite_color => 'gray and blue',
+#     :favorite_sport => 'Real Football',
+#     :favorite_jam => 'Computer Blue',
+#     :what_am_i_gonna_kick => 'Web Developments Ass'
+#   }.to_json
+# end
 
 
 # Bundle exec rackup  // Turns your server on
